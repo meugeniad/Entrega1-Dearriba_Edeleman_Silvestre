fbfacf1a5794c562f49654b69f83153bf9e4c615
Pablo Silvestre <pablosilvestre.tai@gmail.com>
2022-10-16 20:16:10 -0300

    Se arregla formulario

--- home/forms.py
+++ home/forms.py
@@ -4,8 +4,8 @@ from django import forms
 class DestinoFormulario(forms.Form):
     pais = forms.CharField(max_length = 50)
     ciudad = forms.CharField(max_length = 50)
-    informacion =  forms.CharField(max_length = 500)
-    sugerido_para = forms.CharField(max_length = 200)
+    informacion = forms.CharField(max_length=500, widget=forms.Textarea)
+    sugerido_para = forms.CharField(max_length = 100)
     autor = forms.CharField(max_length = 50)
     
 class BuscarPaisFormulario(forms.Form):
